9278c04fa403e63436a8b326030897b370222659
Gabrielagm <gabrielagomezmilla@gmail.com>
2017-11-20 11:54:35 -0500

    añade correción a js

--- js/app.js
+++ js/app.js
@@ -1,59 +1,59 @@
+window.addEventListener('load', function(event) {
+  var boxMain = document.getElementById('box-main');
+  var inputMain = document.getElementById('input-main');
+  var form1 = document.getElementById('form1');
+  var form1Button = document.getElementById('form1-button');
+  var form1Input = document.getElementById('form1-input');
 
-var boxMain = document.getElementById('box-main');
-var inputMain = document.getElementById('input-main');
-var form1 = document.getElementById('form1');
-var form1Button = document.getElementById('form1-button');
-var form1Input = document.getElementById('form1-input');
+  // en esta primera parte me valgo del uso de las clases para ocultar y mostrar los elementos
+  inputMain.addEventListener('click', function() {
+    if (form1.classList.contains('oculto')) {
+      form1.classList.remove('oculto');    
+    }
+    boxMain.replaceChild(form1, inputMain);
+    // evento para la creación de nuevo y su inserción, cada vez que se de click al botón del formulario(form1)
 
-// en esta primera parte me valgo del uso de las clases para ocultar y mostrar los elementos
-inputMain.addEventListener('click', function() {
-  if (form1.classList.contains('oculto')) {
-    form1.classList.remove('oculto');    
-  }
-  boxMain.replaceChild(form1, inputMain);
-  // evento para la creación de nuevo y su inserción, cada vez que se de click al botón del formulario(form1)
+    form1Button.addEventListener('click', function() {
+      var keep = boxMain.removeChild(form1);
+      var boxList = document.createElement('div');
+      var boxListHeader = document.createElement('div');
+      var boxListContent = document.createElement('div');
+      boxList.appendChild(boxListHeader);
+      boxList.appendChild(boxListContent);
+      boxListHeader.textContent = form1Input.value;
+      // para limpiar el formulario para una nueva entrada
+      form1Input.value = '';
+      boxListContent.textContent = 'Añadir una tarjeta...';
+      // dando clases para el estilo de los elementos
+      boxList.classList.add('boxListStyle');
+      boxListHeader.classList.add('boxListHeaderStyle');
+      boxListContent.classList.add('boxListContentStyle');
+      // primero se anexa el elemento creado y después el input, para tener la opción de ingresar una nueva lista.
+      boxMain.appendChild(boxList);
+      boxMain.appendChild(inputMain);
+      // evento para la creación de textarea para ingresar una tarjeta.  
+      boxListContent.addEventListener('click', function(event) {
+        var divList = document.createElement('div');
+        var textArea = document.createElement('textarea');
+        var buttonDivList = document.createElement('button');
+        var spanDivList = document.createElement('span');
+        divList.appendChild(textArea);
+        divList.appendChild(buttonDivList);
+        divList.appendChild(spanDivList);
+        divList.classList.add('divListStyle');
+        textArea.classList.add('textAreaStyle');
+        buttonDivList.classList.add('buttonDivListStyle');
+        spanDivList.classList.add('spanDivListStyle')
+        spanDivList.textContent = 'X';
+        buttonDivList.textContent = 'Guardar';
+        // se le da nuevo estilo height = 'auto' para que cada vez que se anexe el nuevo elemento, boxListContent se adapte a el. También se limpia el contenido para ingresar una nueva tarjeta.
+        // y por último se anexa el nuevo elemento. 
+        boxListContent.style.height = 'auto';
+        boxListContent.textContent = '';
+        boxList.appendChild(divList);
 
-  form1Button.addEventListener('click', function() {
-    var keep = boxMain.removeChild(form1);
-    var boxList = document.createElement('div');
-    var boxListHeader = document.createElement('div');
-    var boxListContent = document.createElement('div');
-    boxList.appendChild(boxListHeader);
-    boxList.appendChild(boxListContent);
-    boxListHeader.textContent = form1Input.value;
-    // para limpiar el formulario para una nueva entrada
-    form1Input.value = '';
-    boxListContent.textContent = 'Añadir una tarjeta...';
-    // dando clases para el estilo de los elementos
-    boxList.classList.add('boxListStyle');
-    boxListHeader.classList.add('boxListHeaderStyle');
-    boxListContent.classList.add('boxListContentStyle');
-    // primero se anexa el elemento creado y después el input, para tener la opción de ingresar una nueva lista.
-    boxMain.appendChild(boxList);
-    boxMain.appendChild(inputMain);
-    // evento para la creación de textarea para ingresar una tarjeta.  
-    boxListContent.addEventListener('click', function(event) {
-      var divList = document.createElement('div');
-      var textArea = document.createElement('textarea');
-      var buttonDivList = document.createElement('button');
-      var spanDivList = document.createElement('span');
-      divList.appendChild(textArea);
-      divList.appendChild(buttonDivList);
-      divList.appendChild(spanDivList);
-      divList.classList.add('divListStyle');
-      textArea.classList.add('textAreaStyle');
-      buttonDivList.classList.add('buttonDivListStyle');
-      spanDivList.classList.add('spanDivListStyle')
-      spanDivList.textContent = 'X';
-      buttonDivList.textContent = 'Guardar';
-      // se le da nuevo estilo height = 'auto' para que cada vez que se anexe el nuevo elemento, boxListContent se adapte a el. También se limpia el contenido para ingresar una nueva tarjeta.
-      // y por último se anexa el nuevo elemento. 
-      boxListContent.style.height = 'auto';
-      boxListContent.textContent = '';
-      boxList.appendChild(divList);
-
-      /* Esta parte está aún por definir.
-      textArea.addEventListener('keydown', function(){
+        /* Esta parte está aún por definir.
+        textArea.addEventListener('keydown', function(){
                 var count = 0;
                 count = textArea.value.length + 1;
                 if(count === 0){
@@ -66,19 +66,19 @@ inputMain.addEventListener('click', function() {
                 }  
         })
     */
-      // evento para crear y anexar el elemento que me permitirá guardar y mostrar la tarea ingresada.
-      buttonDivList.addEventListener('click', function() {    
-        var newDiv = document.createElement('div');
-        newDiv.classList.add('newDivStyle');
-        boxList.style.height = 'auto';
-        newDiv.textContent = textArea.value;
-        boxListContent.appendChild(newDiv);
-        textArea.value = '';                  
+        // evento para crear y anexar el elemento que me permitirá guardar y mostrar la tarea ingresada.
+        buttonDivList.addEventListener('click', function() {    
+          var newDiv = document.createElement('div');
+          newDiv.classList.add('newDivStyle');
+          boxList.style.height = 'auto';
+          newDiv.textContent = textArea.value;
+          boxListContent.appendChild(newDiv);
+          textArea.value = '';                  
+        });
       });
     });
   });
 });
-
 
 /*
 boxListContent.addEventListener('click', function(){
